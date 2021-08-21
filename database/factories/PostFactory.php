be68e22a2b06719874c22824295ce150ed92e302
fumiaki23 <1117fumiaki@gmail.com>
2021-08-21 16:13:00 +0900

    [add]モデル、コントローラーの作成およびファクトリーでのテストデータの追加

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Post;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->word,
+        'place' => $faker->word,
+        'applicants' => $faker->randomDigitNotNull,
+        'body' => $faker->realText,
+        'name' =>$faker->userName
+    ];
+});
