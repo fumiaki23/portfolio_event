abc36c698f1a554b54ea6643d1a0bfd5ae8cea0b
fumiaki23 <1117fumiaki@gmail.com>
2021-08-23 21:30:15 +0900

    [add]イベント投稿画面の追加

--- app/Post.php
+++ app/Post.php
@@ -10,4 +10,13 @@ class Post extends Model
     {
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
+    
+    protected $fillable = [
+        'title',
+        'place',
+        'applicants',
+        'body',
+        'name',
+        'content'
+    ];
 }
---
+++ resources/views/create.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Event</title>
+    </head>
+    <body>
+        <h1>Event Name</h1>
+        <form action="/posts" method="POST">
+            @csrf
+            <div class="title">
+                <h2>タイトル</h2>
+                <input type="text" name="post[title]" placeholder="タイトル"/>
+            </div>
+            <div clss="place">
+                <h2>都道府県</h2>
+                <input type="text" name="post[place]" placeholder="都道府県"/>
+            </div>
+            <div class="applicants">
+                <h2>募集人数</h2>
+                <input type="number" name="post[applicants]"/>
+            </div>
+            <div class="body">
+                <h2>概要</h2>
+                <textarea name="post[body]" placeholder="一覧ページに表示されます。"></textarea>
+            </div>
+            <div class="name">
+                <h2>ユーザーネーム</h2>
+                <input type="text" name="post[name]" placeholder="ああああ"/>
+            </div>
+            <div class="content">
+                <h2>詳細情報</h2>
+                <textarea name="post[content]" placeholeder="詳細画面に表示されます。"></textarea>
+            </div>
+            <input type="submit" value="保存"/>
+        </form>
+        <div class="back">[<a href="/">back</a>]</div>
+    </body>
+</html>
